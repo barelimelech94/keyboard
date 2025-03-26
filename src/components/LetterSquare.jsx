2cbd133c30b724a8cb828b93bf52d089c9519002
Bar Elimelech <bar@cognata.com>
2025-03-26 15:17:28 +0200

    - Simplify LetterSquare component

--- src/components/LetterSquare.jsx
+++ src/components/LetterSquare.jsx
@@ -2,9 +2,5 @@ import React from 'react';
 import '../styles/LetterSquare.css';
 
 export const LetterSquare = ({ letter, status }) => {
-    const classNames = ['letter-square'];
-    if (status === 'success') classNames.push('success');
-    if (status === 'error') classNames.push('error');
-
-    return <div className={classNames.join(' ')}>{letter}</div>;
+    return <div className={`letter-square ${status}`}>{letter}</div>;
 };
